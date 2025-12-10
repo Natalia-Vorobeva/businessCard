08bcd08e264b3fb15c91a73d47e2da5f71fb0f33
Natalia <vorobjeva.natalia76@gmail.com>
2025-12-10 10:22:10 +0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/businessCard/",
+	base: "/",
 	plugins: [react()],
 	// envPrefix: ['VITE_'],
 })
